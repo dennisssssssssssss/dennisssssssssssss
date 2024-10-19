f7785dba263d040d1c7b5bf6b3c2994edf55e37b
dennisssssssssssss <163263559+dennisssssssssssss@users.noreply.github.com>
2024-10-19 19:04:52 +0300

    Create Lab2.cpp

---
+++ Facultate/Lab2.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+
+      /*EX 11
+    float x, rezultat;
+
+    // Citirea numărului real de la tastatură
+    printf("Introduceți un număr real x: ");
+    scanf("%f", &x);
+    rezultat =((8*x - 16)/(x*x+x+1))*((x*x-1)/(x*x+3*x+5));
+    printf("Rezultatul expresiei este: %.2f\n", rezultat);*/
+
+     /*Ex 12
+     int a,r;
+     float A,L,pi;
+     printf("raza cercului este :");
+     scanf("%d",&r);
+     pi=3.14;
+     A=pi*r*r;
+     L=2*pi*r;
+     printf("Lungimea cercului este :%.3f\nAria cercului este :%.3f",L,A);*/
+
+
+       /*EX 13
+     char un_caracter='a';
+     int un_numar=2,vector[10]={5};
+     float un_numar_real=3.14;
+     double un_alt_numar_real=5.654;
+     long int x;
+     long long y;
+     printf("%zu octeti",sizeof(un_caracter));
+     printf("\n%zu octeti",sizeof(un_numar));
+     printf("\n%zu octeti",sizeof(vector));
+     printf("\n%zu octeti",sizeof(un_numar_real));
+     printf("\n%zu octeti",sizeof(un_alt_numar_real));
+     printf("\n%zu octeti",sizeof(x));
+     printf("\n%zu octeti",sizeof(y));*/
+
+        /*Ex 14
+      time_t current_time;
+      current_time=time(NULL);
+      printf("CARNET NOTE\n");
+      printf("Data si ora curenta: %s",ctime(&current_time));
+      printf("Student : Andronic Denis\n");
+      printf("Nr.\tCrt.\tDisciplina\tNota examen\n--------------------------------------------\n");
+      float a,b,c;
+      printf("1.\tPCLP\t\t\t: ");
+      scanf("%f",&a);
+      printf("2.\tGAC\t\t\t: ");
+      scanf("%f",&b);
+      printf("3.\tAM\t\t\t: ");
+      scanf("%f",&c);
+      printf("Media pentru semestrul I este : %.2f",(a+b+c)/3);*/
+
+
+
+    return 0;
+}
